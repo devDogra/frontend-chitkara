a44417e0636762419e77d7ef2a2d0d6a1aef0dbd
Dev Aryan Dogra <devdogra1@gmail.com>
2023-08-17 20:00:26 +0530

    add remove functionality

--- Lecture21_TodoList/script.js
+++ Lecture21_TodoList/script.js
@@ -1,15 +1,43 @@
 const input = document.querySelector("input"); 
-const btn = document.querySelector("button"); 
+const btn = document.querySelector(".add"); 
 const list = document.querySelector("ul"); 
 
 function addTodo() {
     const todoName = input.value; 
 
     const newLi = document.createElement("li");
-    newLi.textContent = todoName; 
+    const newLi_p = document.createElement("p"); 
+    const newLi_del = document.createElement("button");
 
+    // newLi.textContent = todoName; 
+    newLi_p.textContent = todoName; 
+    newLi_del.textContent = "Delete"; 
+    newLi_del.classList.add("del");
+
+    // newLi_del.onclick = ...
+
+    newLi.appendChild(newLi_p);
+    newLi.appendChild(newLi_del);
     list.appendChild(newLi)
     input.value = ""; 
 }
 
 btn.addEventListener("click", addTodo);
+
+function removeTodo(event) {
+    const clicked = event.target; 
+
+    const clickedIsDelBtn = clicked.matches(".del");
+    if (clickedIsDelBtn == false) {
+        return; 
+    } else {
+        console.log(clicked); 
+        const li = clicked.parentElement; 
+
+        li.remove(); 
+
+    }
+}
+
+list.addEventListener("click", removeTodo);
+
