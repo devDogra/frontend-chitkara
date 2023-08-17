8496720a68b211853240f57f522f8a10803ff044
Dev Aryan Dogra <devdogra1@gmail.com>
2023-08-17 15:30:24 +0530

    ext. l21

--- Lecture21_TodoList/script.js
+++ Lecture21_TodoList/script.js
@@ -6,10 +6,27 @@ function addTodo() {
     const todoName = input.value; 
 
     const newLi = document.createElement("li");
-    newLi.textContent = todoName; 
+    const newLi_p = document.createElement("p");
+    const newLi_del = document.createElement("button"); 
+
+    newLi_del.classList.add("del-btn");
+    newLi_del.textContent = "Delete"; 
+    newLi_p.textContent = todoName; 
+
+    newLi.appendChild(newLi_p);
+    newLi.appendChild(newLi_del);
 
     list.appendChild(newLi)
     input.value = ""; 
 }
 
 btn.addEventListener("click", addTodo);
+
+function removeTodo(event) {
+    // Select the clicked list item 
+    const clicked = event.target; 
+    console.log(clicked); 
+
+}
+
+list.addEventListener("click", removeTodo);
