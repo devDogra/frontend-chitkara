0b7366ab772c4258104fa862c79886fffa67d5de
Dev Aryan Dogra <devdogra1@gmail.com>
2023-10-13 14:47:13 +0530

    usestate

--- g18-react/src/Task.jsx
+++ g18-react/src/Task.jsx
@@ -1,10 +1,23 @@
+import { useState } from "react";
+
 function Task(props) {
+    // let taskDone = false; 
+    let [taskDone, setTaskDone] = useState(false)
+
+
+    function handleCheck(event) {
+      console.log("handleCheck");
+      const checkbox = event.target.checked; 
+      // taskDone = checkbox;
+      setTaskDone(checkbox);
+    }
+
     return (
-      <li className="task">
+      <li className={ taskDone ? "task done" : "task" }>
         <h3> { props.taskname } </h3>
         <p> { props.desc } </p>
         <p> IMPORTANCE - { props.importance } </p>
-        <input type="checkbox"></input>
+        <input type="checkbox" onChange={handleCheck()}></input>
       </li>
     );
 }
