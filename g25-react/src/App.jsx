a7f1ae631deac53ab817e6068b006278a040d739
Dev Aryan Dogra <devdogra1@gmail.com>
2023-10-16 13:15:23 +0530

    g25 react handlechange

--- g25-react/src/App.jsx
+++ g25-react/src/App.jsx
@@ -2,13 +2,25 @@ import "./App.css"
 import Task from "./Task";
 
 function App() {
+  const tasks = [
+    { taskName: "Study", important: true, id: 100 },
+    { taskName: "Shopping", important: false, id: 101 },
+    { taskName: "Work out", important: true, id: 102 },
+  ]
   return (
     <>
       <h1>Todo List</h1>
       <ul>
-        <Task></Task>
-        <Task></Task>
-        <Task></Task>
+          {
+              tasks.map(taskObj => {
+                return <Task 
+                    taskName={taskObj.taskName} 
+                    important={taskObj.important}
+                    key={taskObj.id}
+                    >
+                  </Task>
+              })
+          }
       </ul>
     </>
   );
--- g25-react/src/Task.jsx
+++ g25-react/src/Task.jsx
@@ -1,8 +1,24 @@
-function Task() {
+function Task(props) {
+
+ 
+    function handleChange(event) {
+      console.log("Changed"); 
+      const checked = event.target.checked;
+      console.log(checked); 
+      const li = event.target.parentElement; 
+
+      if (checked) {
+        li.classList.add("done"); 
+      } else {
+        li.classList.remove("done"); 
+      }
+
+    }
+
     return (
-      <li className="task">
-        <h2>Task Name</h2>
-        <input type="checkbox"></input>
+      <li className={ props.important ? "task important" : "task"}>
+        <h2>{ props.taskName }</h2>
+        <input type="checkbox" onChange={handleChange}></input>
       </li>
     );
 }
