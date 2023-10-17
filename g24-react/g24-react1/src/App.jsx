74a5be9492210a3737d431373532b978b224ff9a
Dev Aryan Dogra <devdogra1@gmail.com>
2023-10-17 16:54:12 +0530

    g24-react1

--- g24-react/g24-react1/src/App.jsx
+++ g24-react/g24-react1/src/App.jsx
@@ -2,13 +2,30 @@ import "./App.css"
 import Task from "./Task";
 
 function App() {
+  let tasks = [
+    { id: 101, taskName: "Study", desc: "Study at 8PM", imp: false },
+    { id: 102, taskName: "Sleep", desc: "Sleep at 10PM", imp: true },
+    { id: 103, taskName: "Workout", desc: "Workout at 10AM", imp: false },
+  ]
+
+    
   return (
-    <ul>
-      <Task></Task>
-      <Task></Task>
-      <Task></Task>
-      <Task></Task>
-    </ul>
+    <>
+      <h1>Todo List</h1>
+      <ul>
+         {
+            tasks.map((task) => {
+              return <Task 
+                  taskName={task.taskName} 
+                  desc={task.desc}
+                  imp={task.imp}
+                  key={task.id}
+                ></Task>
+            })
+         }
+      </ul>
+    </>
+
   );
 
 }
--- g24-react/g24-react1/src/Task.jsx
+++ g24-react/g24-react1/src/Task.jsx
@@ -1,9 +1,23 @@
-function Task() {
+
+function Task(props) {
+    function handleCheck(event) {
+      const checkbox = event.target; 
+      const checked = checkbox.checked; 
+      console.log(checked);
+
+      if (checked) {
+        checkbox.parentElement.classList.add("done");
+      } else {
+        checkbox.parentElement.classList.remove("done"); 
+      }
+    }
+
     return (
-      <li className="task">
-        <h3>Task Name</h3>
-        <p>Description</p>
-        <input type="checkbox"></input>
+      <li className={ props.imp ? "task important" : "task"}>
+        <h3>{ props.taskName }</h3>
+        <p>{ props.desc }</p> 
+        <p>SUM =  100 + 200 </p>
+        <input onChange={handleCheck} type="checkbox"></input>
       </li>
     );
 }
